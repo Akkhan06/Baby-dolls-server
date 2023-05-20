c9ad52d8077d6c99b8dce9abcdc159b46369afa8
Akkhan601~ <ak01648065092@example.com>
2023-05-20 17:47:40 +0600

    delete function added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
 
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5stkogd.mongodb.net/?retryWrites=true&w=majority"`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5stkogd.mongodb.net/?retryWrites=true&w=majority`
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,58 +28,84 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
 
-//  const subCollection = client.db('subCategory').collection('subItems')
+// DATA COLLECTION FUNCTION
  const AllToysCollection = client.db('subCategory').collection('AllToysItems')
 
-//  app.get('/subcategory_all', async(req, res) => {
-//     const result = await subCollection.find().toArray()
-//     res.send(result)
-//  })
 
-//  app.get('/subcategory_single/:id', async(req, res) => {
-//     const id = req.body;
-//     console.log(id)
-//  })
 
-//  Post data 
+//  POST DATA  
  app.post('/alltoys', async(req, res) => {
     const query = req.body;
-    console.log(query)
+    console.log('line 47',query)
     const result = await AllToysCollection.insertOne(query)
     res.send(result)
  })
 
 // GET TOYS FROM TOYS COLLECTION 
 app.get('/alltoys', async(req, res) => {
-    const query = req.body;
     const result = await AllToysCollection.find({}).toArray()
     res.send(result)
 })
 
 
+// SUB CATEGORY SHOW SUM DATA FUNCTION 
 app.get('/alltoys_text/:text', async(req, res) => {
   const doc = req.params.text;
   if(doc =='csd' || doc == 'afd' || doc == 'abd'){
     const result = await AllToysCollection.find({category: req.params.text}).toArray()
    return res.send(result)
   }
-  
   const result = await AllToysCollection.find({}).toArray()
     res.send(result)
 })
 
+
+// GET DATA BY EMAIL FUNCTION
 app.get('/alltoys_email/:email', async(req, res) => {
   console.log(req.params.email)
   const result = await AllToysCollection.find({email: req.params.email}).toArray()
   res.send(result)
 })
 
+
+// DELETE ONE DATA FROM ALL DATA
+app.delete('/alltoys_email/:id', async(req, res) => {
+  const id = req.params.id;
+  const query = {_id: new ObjectId(id)}
+  const result = await AllToysCollection.deleteOne(query)
+  res.send(result)
+})
+
+
+// GET ONE DATA FROM ALL DATA
 app.get("/alltoys_one/:id", async(req, res) => {
   const id = req.params.id;
   const query = {_id: new ObjectId(id)}
   const result = await AllToysCollection.findOne(query)
   res.send(result)
 })
+
+
+// UPDATE ONE DATA FROM ONE DATA
+app.put('/alltoys_one/:id', async(req, res) => {
+  const id = req.params.id;
+  const filter = {_id: new ObjectId(id)}
+  const options = { upsert: true }
+  const updatedInfo = req.body;
+  console.log("hello",updatedInfo)
+  const updateDoc = {
+    $set: {
+      price: updatedInfo.price,
+      quantity: updatedInfo.quantity,
+      discription: updatedInfo.discription
+    },
+  };
+  const result = await AllToysCollection.updateOne(filter, updateDoc, options);
+  res.send(result)
+})
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
